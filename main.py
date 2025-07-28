a0ac0c0b46b6d8a0310f63dbd33787c187556379
Ehsan-66 <ehsan.zamanlou.68@gmail.com>
2025-07-28 23:34:23 +0100

    Update main.py
    test message

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# main bot logic here
+import os
+from telegram import Bot
+
+# گرفتن توکن و چت آیدی از تنظیمات رندر
+bot_token = os.getenv("BOT_TOKEN")
+chat_id = os.getenv("CHAT_ID")
+
+# ساخت ربات
+bot = Bot(token=bot_token)
+
+# ارسال پیام تست به تلگرام شما
+bot.send_message(chat_id=chat_id, text="✅ ربات با موفقیت اجرا شد و به تلگرام وصله!")
